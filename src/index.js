bca1ec3425ea7e73ef5ddac8e212ab35de37c362
SirBepy <tecnomon99@gmail.com>
2025-01-29 19:25:34 +0100

    FIX: attempt to fix sanitization

--- src/index.js
+++ src/index.js
@@ -131,7 +131,13 @@ const parseXml = async (xmlString) => {
 const sanitizeText = (text) => {
   if (!text) return text;
 
-  return text
+  // Decode from Windows-1252 to UTF-8
+  const decoder = new TextDecoder("windows-1252");
+  const encoder = new TextEncoder();
+
+  let decodedText = decoder.decode(encoder.encode(text));
+
+  return decodedText
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
     .replace(/&amp;/g, "&")
